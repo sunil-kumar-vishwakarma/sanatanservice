a9142ac6fc7a9cb42ff406498115a4739fc24ced
Sunil Vishwakarma <sunil.vis2051991@gmail.com>
2025-03-27 17:34:29 +0530

    change url public

--- server.php
+++ server.php
@@ -21,5 +21,5 @@ if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
     return false;
 }
 
-require_once __DIR__.'/index.php';
-// require_once __DIR__.'/public/index.php';
+// require_once __DIR__.'/index.php';
+require_once __DIR__.'/public/index.php';
